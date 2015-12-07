401d77d461b81abb5b72c6e4f968f666d6487b38
Hillel Wayne <hwayne@gmail.com>
2015-12-06 18:16:43 -0600

    proper wsgi

--- ripper/ripper/wsgi.py
+++ ripper/ripper/wsgi.py
@@ -8,9 +8,9 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ripper.settings")
 
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ripper.settings")
+from dj_static import Cling
 
 application = get_wsgi_application()
